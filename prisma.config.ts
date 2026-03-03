c6a68703a4db2c8acb9edab773964858dc5b072a
crypt0ech0x <crypt0ech0x@gmail.com>
2026-03-02 22:24:11 -0800

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,6 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   datasource: {
-    // Must be a DIRECT Postgres URL (not prisma+postgres)
     url: env("DATABASE_URL"),
   },
 });
