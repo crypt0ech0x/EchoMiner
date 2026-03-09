946227ed6545bca44ae6d71be8a06ab9a71b9b38
crypt0ech0x <crypt0ech0x@gmail.com>
2026-03-08 23:07:24 -0700

    Create store-packages.ts

---
+++ lib/store-packages.ts
@@ -0,0 +1,20 @@
+export const STORE_PACKAGES = [
+  {
+    id: "starter",
+    name: "Starter Pack",
+    solAmount: 0.1,
+    echoAmount: 100,
+  },
+  {
+    id: "pro",
+    name: "Pro Pack",
+    solAmount: 0.5,
+    echoAmount: 600,
+  },
+  {
+    id: "whale",
+    name: "Whale Pack",
+    solAmount: 1.0,
+    echoAmount: 1400,
+  },
+];
