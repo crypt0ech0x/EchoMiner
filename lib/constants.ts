4bb351bfa1c07fd1b3c38e8ecbe8e65cc3630103
crypt0ech0x <crypt0ech0x@gmail.com>
2026-03-09 23:46:41 -0700

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 
-import { StoreItem } from './types';
+import { StoreItem } from '@/lib/types';
 
 export const BASE_MINING_RATE = 0.0001543;
 export const SESSION_DURATION_MS = 24 * 60 * 60 * 1000;
